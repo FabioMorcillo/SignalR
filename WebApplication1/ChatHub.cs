823be35d7ed429128d6e84e9dcc8b54b63b94c7c
Fabio Morcillo do Nascimento <fabio_morc@hotmail.com>
2016-03-24 11:31:44 -0300

    Primeiro commit

---
+++ WebApplication1/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication1
+{
+    public class ChatHub : Hub
+    {
+        public void enviar(string nome, string mensagem)
+        {
+            if ( mensagem.Length > 0 )
+            {
+                Clients.All.broadcastMensagem(nome, mensagem);
+            }
+        }
+    }
+}
---
+++ WebApplication1/scripts/MeuJavaScript.js
@@ -0,0 +1,78 @@
+﻿$(document).ready(function () {
+
+    var chat = $.connection.chatHub;
+
+    chat.client.broadcastMensagem = function (nome, mensagem) {
+
+        var guardaNome = nome;
+        var guardaMensagem = mensagem;
+
+        if ( nome === "" )
+        {
+            $("#mensagemChat").append("<li>" + guardaMensagem + "</li>");
+        }
+        else
+        {
+            $("#mensagemChat").append("<li><strong>" + guardaNome + "</strong>&nbsp;&nbsp;" + guardaMensagem + "</li>");
+        }
+
+        var objDiv = document.getElementById("mensagemChat");
+        objDiv.scrollTop = objDiv.scrollHeight;
+    }
+
+    var nomePrompt = prompt("Qual seu nome: ", "");
+
+    if ( nomePrompt === "" || !nomePrompt  )
+    {
+        alert("É necessário informar um nome !");
+
+        window.location = "index.html";
+
+        return;
+    }
+    else
+    {
+        $("#apelido").append("Bem vindo " + nomePrompt);
+    }
+
+    $("#mensagem").focus();
+
+    
+
+    $.connection.hub.start().done(
+
+        function () {
+
+            window.onbeforeunload = function (e) {
+                chat.server.enviar("", "<font color=red>" + nomePrompt + " saiu do chat</font>");
+            };
+
+
+            chat.server.enviar("", "<font color=blue>" + nomePrompt + " entrou no chat</font>");
+
+        $("#btnEnviar").click(function () {
+
+            chat.server.enviar(nomePrompt, $("#mensagem").val())
+
+            $("#mensagem").val("").focus();
+
+        })
+
+        // Validando o botão enter
+        $(document).keypress(function (e)
+        {
+            if (e.which == 13)
+            {
+                chat.server.enviar(nomePrompt, $("#mensagem").val())
+
+                $("#mensagem").val("").focus();
+            }
+        });
+
+
+        })
+
+
+    
+
+})
